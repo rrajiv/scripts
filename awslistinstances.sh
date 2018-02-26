bfafe2bc90c4313861b2a536baf092d87c573de0
rajiv <rajiv@utmail.net>
2018-02-25 18:15:39 -0800

    no message

---
+++ awslistinstances.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# script to list the instances running in all regions
+# Pre-requisite for this: Assuming you have the following:
+# 1) Installed the aws cli
+# 2) run aws configure to set up the credentials
+# 3) Output is in JSON
+
+if test $# -ne 0
+then
+	echo "Usage: ./awslistinstances.sh"
+	exit -1
+fi
+
+for r in ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2
+do    
+    echo "Checking region... $r"
+    aws ec2 describe-instances --region $r
+done

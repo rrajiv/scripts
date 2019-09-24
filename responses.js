7d56d3f5ff31ac45e7ba548de8cef100108ad4bc
Rajiv Ramnath <>
2019-09-23 17:23:47 -0700

    no message

--- responses.js
+++ responses.js
@@ -7,7 +7,7 @@ function senderror(req, res, next)
 	var respcode = req.params.responsecode;
 
     // send the cache control header
-    res.header('Cache-control', 'max-age=20s');
+//    res.header('Cache-control', 'max-age=20s');
 
 	// do some selective response codes	
 	if (respcode == "404")
